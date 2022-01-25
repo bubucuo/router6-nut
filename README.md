70bee7a2a3aabdefe3a9e7653b857655a31c3a8d
bubucuo <276185876@qq.com>
2022-01-25 18:50:39 +0800

    save

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# 切分支
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/mini-router6/Route.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function Route({ element, children }) {
-  console.log("children", element); //sy-log
-
-  return element;
-  // return children ? React.cloneElement(element, {}, children) : element;
-}
