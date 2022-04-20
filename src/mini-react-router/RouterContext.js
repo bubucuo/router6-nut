20c5d08470529c5beb6f0ff81b8f3d17abf83864
bubucuo <276185876@qq.com>
2022-04-20 18:11:13 +0800

    init

--- src/mini-react-router/RouterContext.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-
-const RouterContext = React.createContext();
-export default RouterContext;
--- src/mini-react-router/hooks.js
+++ src/mini-react-router/hooks.js
@@ -1,6 +1,3 @@
-import { useContext } from "react";
-import RouterContext from "./RouterContext";
-
 export function useRoutes(routes) {
   const pathname = window.location.pathname;
 
@@ -8,11 +5,4 @@ export function useRoutes(routes) {
     const match = pathname === route.path || pathname === "/" + route.path;
     return match ? route.element : null;
   });
-}
-
-export function useNavigate() {
-  //
-  const { navigator } = useContext(RouterContext);
-
-  return navigator.push;
 }
